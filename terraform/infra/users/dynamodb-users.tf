83c488441f2cb2a5a0f82117b19ccbbb7aa4bb5e
douglas manicka <douglasmanicka@gmail.com>
2020-05-26 23:43:42 -0300

    initial configuration  - terraform/user module

---
+++ terraform/infra/users/dynamodb-users.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "users" {
+  name = "${var.environment}-users"
+  hash_key = "id"
+  attribute {
+      name = "id"
+      type = "S"
+  }
+
+  read_capacity = "${var.read_capacity}"
+  write_capacity = "${var.write_capacity}" 
+}   
+
+resource "aws_ssm_parameter" "dynamodb_users_table" {
+  name = "${var.environment}-dynamodb-users-table" 
+  type = "String"
+  value = "${aws_dynamodb_table.users.name}"
+
+}
+
