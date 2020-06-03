0ab33d9fbc8fe6500925f9f7fd0179b0ded9bc28
douglas manicka <douglasmanicka@gmail.com>
2020-06-02 23:31:58 -0300

    refactor to use terraform  templates

--- terraform/infra/users/iam-policy-login.tf
+++ terraform/infra/users/iam-policy-login.tf
@@ -2,27 +2,8 @@ resource "aws_iam_policy" "login_policy" {
   name        = "${var.environment}-login-policy"
   description = "My test policy"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "dynamoDb:Query"
-      ],
-      "Effect": "Allow",
-      "Resource": "${aws_dynamodb_table.users.arn}/index/${var.environment}-email-gsi"
-    },
-    {
-      "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": "*"      
-    }
-  ]
-}
-EOF
+  policy = templatefile("${path.module}/templates/dynamodb-policy.tpl",{
+    action = "dynamodb:Query",
+    resource =  "${aws_dynamodb_table.users.arn}/index/${var.environment}-email-gsi"
+  })
 }
--- terraform/infra/users/iam-policy-register.tf
+++ terraform/infra/users/iam-policy-register.tf
@@ -2,27 +2,9 @@ resource "aws_iam_policy" "register_policy" {
   name        = "${var.environment}-register-policy"
   description = "My test policy"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "dynamoDb:PutItem"
-      ],
-      "Effect": "Allow",
-      "Resource": "${aws_dynamodb_table.users.arn}"
-    },
-    {
-      "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": "*"      
-    }
-  ]
-}
-EOF
+
+  policy = templatefile("${path.module}/templates/dynamodb-policy.tpl",{
+    action = "dynamodb:PutItem",
+    resource =  "${aws_dynamodb_table.users.arn}"
+  })
 }
