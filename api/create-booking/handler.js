cd5fbee52db5dc71124b6bd0c1d498760f726326
douglas manicka <douglasmanicka@gmail.com>
2020-06-04 13:07:36 -0300

    implementing the reserve creation function

--- api/create-booking/handler.js
+++ api/create-booking/handler.js
@@ -1,7 +1,36 @@
-module.exports.create = async event => {
+const AWS = require('aws-sdk')
+AWS.config.update({
+    region: process.env.AWS_REGION
+})
+
+const documentClient =  new AWS.DynamoDB.DocumentClient()
+const { v4: uuidv4 } = require('uuid');
+
+module.exports.create = async event => { 
+  const body = JSON.parse(event.body)
+  
+
+  await  documentClient.put({
+       TableName: process.env.DYNAMODB_BOOKINGS,
+       Item: {
+           id: uuidv4(),
+           date: body.date,
+           user: event.requestContext.authorizer,
+           idUser: event.requestContext.authorizer.user.id
+          
+       }
+   }).promise()
     return{
         statusCode: 200,
-        body: JSON.stringify(event)
+        body: JSON.stringify({message: 'Booking Created Successfully!'})
     }
 
 }
+
+// module.exports.create = async event => {
+//     return{
+//         statusCode: 200,
+//         body: JSON.stringify(event)
+//     }
+
+// }
