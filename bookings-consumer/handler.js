e39b3d359fbd21e1788c19ac4c623cf4f559cf77
douglas manicka <douglasmanicka@gmail.com>
2020-06-08 00:55:01 -0300

    publish message on SNS

--- bookings-consumer/handler.js
+++ bookings-consumer/handler.js
@@ -1,17 +1,29 @@
 'use strict';
 
 const AWS =  require('aws-sdk')
+AWS.config.update({
+  region: process.env.AWS_REGION
+})
+
+const SNS =  new AWS.SNS()
 const converter =  AWS.DynamoDB.Converter 
 const moment = require('moment')
 moment.locale('en')
 
 
 module.exports.listen = async event => {
+  const snsPromises = []
   for (const record of event.Records) {
     if (record.eventName === 'INSERT') {
       const reserva = converter.unmarshall(record.dynamodb.NewImage)
-      console.log(`Reservation made: the user ${reserva.user.name} - ${reserva.user.email} has scheduled a time : ${moment(reserva.date).format('LLLL')}`)
+      // console.log(`Reservation made: the user ${reserva.user.name} - ${reserva.user.email} has scheduled a time : ${moment(reserva.date).format('LLLL')}`)
+      snsPromises.push(SNS.publish({
+        TopicArn: process.env.SNS_NOTIFICATIONS_TOPIC,
+        Message: `Reservation made: the user ${reserva.user.name} - ${reserva.user.email} has scheduled a time : ${moment(reserva.date).format('LLLL')}`
+      }).promise())
     }
   }
+  await Promise.all(snsPromises)
+  console.log("successfully sent messages")
   return { message: 'Function executed successfully!', event };
 };
