dd81a4b3a9e879fd5c04c852681e73ee9948ec8b
douglas manicka <douglasmanicka@gmail.com>
2020-06-07 22:53:49 -0300

    add moment.js / creating booking message

--- bookings-consumer/handler.js
+++ bookings-consumer/handler.js
@@ -2,11 +2,16 @@
 
 const AWS =  require('aws-sdk')
 const converter =  AWS.DynamoDB.Converter 
+const moment = require('moment')
+moment.locale('en')
 
 
 module.exports.listen = async event => {
   for (const record of event.Records) {
-    console.log("log parseado", converter.unmarshall(record.dynamodb.NewImage))
+    if (record.eventName === 'INSERT') {
+      const reserva = converter.unmarshall(record.dynamodb.NewImage)
+      console.log(`Reservation made: the user ${reserva.user.name} - ${reserva.user.email} has scheduled a time : ${moment(reserva.date).format('LLLL')}`)
+    }
   }
   return { message: 'Function executed successfully!', event };
 };
